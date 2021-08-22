10a7a0a5dcf1c973b9f176246e10ef869658876d
Luciano Silva <luciano.silva@cloudmed.io>
2021-08-22 14:09:01 -0300

    creating tests on application layer

---
+++ Api.Application.Test/Usuario/QuandoRequisitarDelete/Retorno_BadRequest.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Api.Application.Controllers;
+using Api.Domain.Dtos.User;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Api.Application.Test.Usuario.QuandoRequisitarDelete
+{
+  public class Retorno_BadRequest
+  {
+    private UsersController _controller;
+    [Fact(DisplayName = "É possivel realizar o Delete")]
+    public async Task E_Possivel_Invocar_Controller_Delete()
+    {
+      var serviceMock = new Mock<IUserService>();
+      var nome = Faker.Name.FullName();
+      var email = Faker.Internet.Email();
+
+      serviceMock.Setup(m => m.Delete(It.IsAny<Guid>()))
+                  .ReturnsAsync(false);
+
+      _controller = new UsersController(serviceMock.Object);
+      _controller.ModelState.AddModelError("Id", "Formato Inválido");
+
+
+      var result = await _controller.Delete(default(Guid));
+      Assert.True(result is BadRequestObjectResult);
+      Assert.False(_controller.ModelState.IsValid);
+    }
+  }
+}
---
+++ Api.Application.Test/Usuario/QuandoRequitarUpdate/Retorno_Updated.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading.Tasks;
+using Api.Application.Controllers;
+using Api.Domain.Dtos.User;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Api.Application.Test.Usuario.QuandoRequitarUpdate
+{
+  public class Retorno_Updated
+  {
+    private UsersController _controller;
+    [Fact(DisplayName = "É possivel realizar o Updated")]
+    public async Task E_Possivel_Invocar_Controller_Updated()
+    {
+      var serviceMock = new Mock<IUserService>();
+      var nome = Faker.Name.FullName();
+      var email = Faker.Internet.Email();
+
+      serviceMock.Setup(m => m.Put(It.IsAny<UserDtoUpdate>())).ReturnsAsync(
+        new UserDtoUpdateResult
+        {
+          Id = Guid.NewGuid(),
+          Name = nome,
+          Email = email,
+          UpdateAt = DateTime.UtcNow
+        }
+      );
+      _controller = new UsersController(serviceMock.Object);
+
+      var userDtoUpdate = new UserDtoUpdate
+      {
+        Id = Guid.NewGuid(),
+        Name = nome,
+        Email = email
+      };
+
+      var result = await _controller.Put(userDtoUpdate);
+      Assert.True(result is OkObjectResult);
+
+      var resultValue = ((ObjectResult)result).Value as UserDtoUpdateResult;
+      Assert.NotNull(resultValue);
+      Assert.Equal(userDtoUpdate.Name, resultValue.Name);
+      Assert.Equal(userDtoUpdate.Email, resultValue.Email);
+
+    }
+  }
+}
